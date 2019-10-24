f27d11649e8feae8056fa3d929852def2183bf49
deleterium <rui.santana@protonmail.com>
2019-10-23 22:04:14 -0300

    Reflecting changes: (GET to POST) (AESencript to AESencriptCTR)

--- signalMsg.js
+++ signalMsg.js
@@ -2,8 +2,11 @@
 // send a signaling message identified by id=0 and extra date paramter
 function sendSignal(msg){
   var msgWithId = "0;&" + JSON.stringify(msg)+"&"+ Date.now();
-  var encMsg = AESencrypt(msgWithId,msgSymKey); //AESencrypt is not defined
-    $.get("sendMsg.php?username=" + inputEmail + "&password=" + authenticationkey + "&user2Id=" + user2Id + "&msg=" + encMsg,
+  var nonce = secureRandom(8);
+  var encMsg = AESencryptCTR(msgWithId,msgSymKey,nonce);
+  var hexNonce = ByteArray_2_HexString(nonce);
+
+  $.post("sendMsg.php", { username: inputEmail, password: authenticationkey,  user2Id: user2Id, msg: encMsg, nonce: hexNonce},
     function(data, status){
       if(data == 1) {
         //console.log(`message sent = ${msgWithId}`);
